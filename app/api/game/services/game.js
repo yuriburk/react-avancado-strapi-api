2d9aa55de29e21ddbdb086427b22f1c081d2f88b
Jefferson Yuri Cunha Burk <jefferson.burk@ambevtech.com.br>
2021-08-27 15:48:22 -0300

    Getting games from GOG api

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -5,6 +5,16 @@
  * to customize this service
  */
 
+const axios = require("axios");
+
 module.exports = {
-  populate: async (params) => {},
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(products);
+  },
 };
