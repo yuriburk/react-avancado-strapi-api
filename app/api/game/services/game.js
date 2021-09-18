29ab3e39f280c41e315e1105513f96167300f9d5
Jefferson Yuri Cunha Burk <jefferson.burk@ambevtech.com.br>
2021-09-18 19:48:28 -0300

    Database

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -6,6 +6,28 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const ratingValue = document.querySelector(".age-restrictions__icon > use")
+    .attributes[0].nodeValue;
+  const rating = ratingValue
+    ? ratingValue.replace("#", "").replace("_", "")
+    : rating;
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating,
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
 
 module.exports = {
   populate: async (params) => {
@@ -14,6 +36,16 @@ module.exports = {
     const {
       data: { products },
     } = await axios.get(gogApiUrl);
+
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase(),
+    });
+
+    await strapi.services.developer.create({
+      name: products[0].developer,
+      slug: slugify(products[0].developer).toLowerCase(),
+    });
 
     console.log(products);
   },
